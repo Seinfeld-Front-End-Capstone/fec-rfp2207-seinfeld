487ea315335d3de6f8d70f0adeeb9eb0d0e55864
Thach Do <thachdo90@gmail.com>
2022-08-24 15:30:28 -0700

    Created directory for Ratings and Reviews components

---
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+class RatingsReviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Section for Ratings and Reviews</h1>
+  }
+
+
+}
