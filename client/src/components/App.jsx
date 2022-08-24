b58f4ab398f9090bf4e34995e70aaddc45a6aece
Kevin <KevinPho1012@gmail.com>
2022-08-24 14:59:19 -0700

    adding overview folder in compnents folder

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Overview from './overview/Overview.jsx';
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Overview/>
+    </div>
+  )
+}
