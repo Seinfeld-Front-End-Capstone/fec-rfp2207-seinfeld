4986b4d7287d04fba014706874a25e76f3230346
Daniel Tan <dantan510@gmail.com>
2022-08-24 17:25:51 -0700

    install react-icons

--- client/src/components/questionsAnswers/QuestionsList.jsx
+++ client/src/components/questionsAnswers/QuestionsList.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+console.log('REACT : ', React);
+
 var QuestionsList = () => {
   return (
     <div>
       <h4>Questions & Answers</h4>
+      <form>
+        <input type='text' placeholder='Have a question? Search for answers...'/>
+        <button className='search-icon' type='submit'></button>
+      </form>
     </div>
   )
 }
