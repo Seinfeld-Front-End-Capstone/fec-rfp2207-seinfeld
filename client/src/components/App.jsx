461149d0adf8e431b536b403268ada331495fe94
Lynn <griff.lynn8@gmail.com>
2022-08-24 17:05:40 -0700

    added RelatedList to App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Overview from './overview/Overview.jsx';
+import RelatedList from './rc/RelatedList.jsx';
 
 export default () => {
   return (
     <div>
       <h1>Hello World</h1>
       <Overview/>
+      <RelatedList />
     </div>
   )
 }
