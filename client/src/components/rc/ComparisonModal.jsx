93a78de79fd694c5efac3bcff9ae71dfd7983271
Lynn <griff.lynn8@gmail.com>
2022-08-26 16:19:05 -0700

    added ComparisonModal file for later implementation

---
+++ client/src/components/rc/ComparisonModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ComparisonModal = () => {
+  //this is a pop-up window that compares the currently viewed item with the related item
+
+  return (
+    <table>
+
+    </table>
+  )
+}
+
+export default ComparisonModal;
