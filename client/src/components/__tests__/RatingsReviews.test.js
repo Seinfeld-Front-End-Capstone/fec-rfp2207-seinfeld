e468d9be8fc666bef5d8a986646c364438086e75
Thach Do <thachdo90@gmail.com>
2022-08-27 12:09:10 -0700

    Added .babelrc and other configurations for jest and RTL in package.json
    Got a very simple test to pass where the text "review" appears somewhere in the RatingsReviews component.
    Had to temporarily disable Stars Component's functionality of importing image because it kept through an error.
    Created a __tests__ to house all test files

--- client/src/components/__tests__/RatingsReviews.test.js
+++ client/src/components/__tests__/RatingsReviews.test.js
@@ -1,10 +1,13 @@
 import {render, fireEvent, screen} from '@testing-library/react'
-import RatingsReviews from '../RatingsReviews/RatingsReviews';
+import RatingsReviews from '../RatingsReviews/RatingsReviews.jsx';
 
 
 describe('Draft test', () => {
   it('tests something', () => {
-    render(<RatingsReviews />);
+    render(<RatingsReviews productId={'65631'}/>);
+
+    const headingText = screen.getAllByText(/review/i)
     expect(true).toBe(true);
+    expect(headingText).toBeTruthy();
   })
 })
