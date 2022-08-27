a05275bd6fd6f17fe4c81372f10c2feb0238d2be
Lynn <griff.lynn8@gmail.com>
2022-08-26 17:37:38 -0700

    refactored avg star rating, now need to figure out how to use with stars helper func

--- client/src/components/helpers/AvgStarRating.jsx
+++ client/src/components/helpers/AvgStarRating.jsx
@@ -10,38 +10,16 @@ import request from '../../request.js';
 const AvgStarRating = ( ratingsMeta, callback ) => {
   var average = 0;
   var count = 0;
-  var one = 0;
-  var two = 0;
-  var three = 0;
-  var four = 0;
-  var five = 0;
+  var stars = 0;
 
   for (var key in ratingsMeta) {
     var star = ratingsMeta[key];
-    console.log('key:', key,'stars:', star);
-    if (key === 1) {
-      one += (star * 1);
-      count += star;
-    } else if (key === 2) {
-      two += (star * 2);
-      count += star;
-    } else if (key === 3) {
-      three += (star * 3);
-      count += star;
-    } else if (key === 4) {
-      four += (star * 4);
-      count += star;
-    } else if (key === 5) {
-      five += (star * 5);
-      console.log('FIVE:', five);
-      count += star;
-    }
+    stars += Number(star) * key;
+    count += Number(star);
   };
 
-  average = one + two + three + four + five / count;
-  console.log("ratingsMeta:", ratingsMeta);
-  console.log('1:', one, '2:', two, '3:', three, '4:', four, '5:', five);
-  console.log('average:', average);
+  average = stars / count;
+   // *** round down 2 decimal to .25, .50, or .75 *** \\
 
   return callback(average);
 }
