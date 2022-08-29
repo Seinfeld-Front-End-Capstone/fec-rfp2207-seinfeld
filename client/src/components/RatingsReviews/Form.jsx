e93e820aaa07085f8bf4a206f4843e7c521305d8
Thach Do <thachdo90@gmail.com>
2022-08-29 16:03:09 -0700

    Added question: do you recommend?

--- client/src/components/RatingsReviews/Form.jsx
+++ client/src/components/RatingsReviews/Form.jsx
@@ -5,6 +5,15 @@ const Form = ({ productName }) => {
     <form>
       <h3>Write your review</h3>
       <p>About the {productName}</p>
+      <p>Overall Rating: select stars</p>
+      <div id="RR-form-recommend">Do you recommend this product?<span aria-label="required">* </span><br/>
+        <input type="radio" id="yes" name="recommend" value="yes" checked required />
+        <label for="yes">yes</label>
+        <input type="radio" id="no" name="recommend" value="no" />
+        <label for="no">no</label>
+      </div>
+      <input type="submit"  value="submit" />
+
     </form>
   )
 }
