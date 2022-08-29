d2b68807346723357ca31b92c452ff832700e008
Lynn <griff.lynn8@gmail.com>
2022-08-29 09:42:13 -0700

    fixed a couple syntax erros in ComparisonModal

--- client/src/components/rc/ComparisonModal.jsx
+++ client/src/components/rc/ComparisonModal.jsx
@@ -4,9 +4,26 @@ const ComparisonModal = () => {
   //this is a pop-up window that compares the currently viewed item with the related item
 
   return (
-    <table>
-
-    </table>
+    <div className="modal">
+   <div className="modal_content">
+     <span className="close">x</span>
+     <p></p>
+     <table>
+    <thead>
+        <tr>
+            <th colSpan="3">Item Comparison</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>The table body</td>
+            <td>with three </td>
+            <td>columns</td>
+        </tr>
+    </tbody>
+</table>
+   </div>
+</div>
   )
 }
 
--- client/src/components/rc/RelatedItemCard.jsx
+++ client/src/components/rc/RelatedItemCard.jsx
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import request from '../../request.js';
-import AvgStarRating from '../helpers/AvgStarRating.jsx';
-import Stars from '../helpers/Stars.jsx';
+import React, { useState, useEffect } from "react";
+import request from "../../request.js";
+import AvgStarRating from "../helpers/AvgStarRating.jsx";
+import Stars from "../helpers/Stars.jsx";
+import ComparisonModal from './ComparisonModal.jsx';
 //takes in the item id and populates each element of the card with relevent info.
 
 const RelatedItemCard = ({ pID }) => {
@@ -9,58 +10,68 @@ const RelatedItemCard = ({ pID }) => {
   const [photoData, setPhotoData] = useState([]);
   const [price, setPrice] = useState([]);
   const [starRating, setStarRating] = useState(0);
+  const [pop, setPop] = useState(false);
 
-  // ****** what to do if item has no photos??? Bright Future Shades ****** \\
 
   useEffect(() => {
-    request.getStyles(pID)
+    request
+      .getStyles(pID)
       .then((data) => {
-        setPhotoData(data.data.results[0].photos[0].url)
+        setPhotoData(data.data.results[0].photos[0].url);
         if (data.data.results[0].sale_price) {
-          setPrice(data.data.results[0].sale_price)
+          setPrice(data.data.results[0].sale_price);
         } else {
-          setPrice(data.data.results[0].original_price)
+          setPrice(data.data.results[0].original_price);
         }
       })
       .catch((err) => {
-        console.log('This is a RelatedItemCard error:', err);
+        console.log("This is a RelatedItemCard error:", err);
       });
-    request.getProductInfo(pID)
+    request
+      .getProductInfo(pID)
       .then((data) => {
         setProductData(data.data);
       })
       .catch((err) => {
-        console.log('This is a RelatedItemCard error:', err);
+        console.log("This is a RelatedItemCard error:", err);
       });
-    request.getReviewMeta(pID)
+    request
+      .getReviewMeta(pID)
       .then((data) => {
         AvgStarRating(data.data.ratings, (avg) => {
           return setStarRating(avg);
-        })
+        });
       })
       .catch((err) => {
-        console.log('This is a RelatedItemCard error:', err);
+        console.log("This is a RelatedItemCard error:", err);
       });
   }, []);
 
-    // need rating stars -- Thach put these somewhere
-    // need little star button -- Might have to grab from font awesome or something similar
-      //on click, the star button opens a pop-up window that shows a comparison between the item on the current overview and the item on the related list.
+  // need little star button -- Might have to grab from font awesome or something similar -- done
+  //on click, the star button opens a pop-up window that shows a comparison between the item on the current overview and the item on the related list.
 
-    // handleStarClick = () => {
-    //   //do stuff. make popup window.
-    // }
+  const handlePopToggle = (event) => {
+    //do stuff. make popup window.
+    event.preventDefault();
+    setPop(true);
+  }
 
   return (
-    <aside>
-      <img src={photoData} height="175"/>
-      <i className="fa-solid fa-star"></i>
-      <h6>{productData.category}</h6>
-      <h5>{productData.name}</h5>
-      <p><small>{price}</small></p>
-      <Stars key={starRating} rating={starRating} />
-    </aside>
-  )
-}
+    <div>
+      <aside>
+        <img src={photoData} height="175" />
+        <i className="fa-solid fa-star" onClick={handlePopToggle}></i>
+        <h6>{productData.category}</h6>
+        <h5>{productData.name}</h5>
+        <p>
+          <small>{price}</small>
+        </p>
+        <Stars rating={starRating} />
+      </aside>
+      {pop ? <ComparisonModal toggle={handlePopToggle} overviewItem={pID} relatedItem={productData} /> : null}
+    </div>
+
+  );
+};
 
 export default RelatedItemCard;
