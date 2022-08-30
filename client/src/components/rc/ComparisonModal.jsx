c9412ea54c9b296952d77b16866dc5079e3b0da4
Lynn <griff.lynn8@gmail.com>
2022-08-29 18:14:12 -0700

    Comparison Modal has been implemented!

--- client/src/components/rc/ComparisonModal.jsx
+++ client/src/components/rc/ComparisonModal.jsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import request from '../../request.js';
 
-const ComparisonModal = () => {
+const ComparisonModal = ({ toggle, overviewItem, relatedItem, relatedPrice, ovPrice }) => {
+
   //this is a pop-up window that compares the currently viewed item with the related item
 
+
+  const handleClick = () => {
+    toggle(false);
+  }
+
+  console.log('related item:', relatedItem);
+  console.log('overview item:', overviewItem);
+
+  const show = toggle ? "RC_show" : "";
+
   return (
-    <div className="modal">
-   <div className="modal_content">
-     <span className="close">x</span>
-     <p></p>
-     <table>
-    <thead>
-        <tr>
-            <th colSpan="3">Item Comparison</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>The table body</td>
-            <td>with three </td>
-            <td>columns</td>
-        </tr>
-    </tbody>
-</table>
-   </div>
-</div>
+    <div className={"RC_modal" + show}>
+      <div className="RC_modal_content">
+        <span className="RC_close" onClick={handleClick}>x</span>
+        <table className="RC_modal_table">
+          <thead>
+            <tr>
+              <th className="RC_modal_td" colSpan="3">Item Comparison</th>
+            </tr>
+            <tr>
+              <th className="RC_modal_td">{overviewItem.name}</th>
+              <th className="RC_modal_td"></th>
+              <th className="RC_modal_td">{relatedItem.name}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="RC_modal_td">${ovPrice}</td>
+              <td className="RC_modal_td">Price</td>
+              <td className="RC_modal_td">${relatedPrice}</td>
+            </tr>
+              {overviewItem.features.map((feat, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="RC_modal_td">{feat.feature}: {feat.value}</td>
+                    <td className="RC_modal_td">Feature</td>
+                    <td className="RC_modal_td">{relatedItem.features[index].feature}: {relatedItem.features[index].value}</td>
+                  </tr>
+                )
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/rc/RelatedItemCard.jsx
+++ client/src/components/rc/RelatedItemCard.jsx
@@ -5,10 +5,12 @@ import Stars from "../helpers/Stars.jsx";
 import ComparisonModal from './ComparisonModal.jsx';
 //takes in the item id and populates each element of the card with relevent info.
 
-const RelatedItemCard = ({ pID }) => {
+const RelatedItemCard = ({ pID, ogID }) => {
   const [productData, setProductData] = useState([]);
+  const [ovItem, setOVItem] = useState([]);
   const [photoData, setPhotoData] = useState([]);
   const [price, setPrice] = useState([]);
+  const [ovItemPrice, setOVItemPrice] = useState([]);
   const [starRating, setStarRating] = useState(0);
   const [pop, setPop] = useState(false);
 
@@ -17,6 +19,7 @@ const RelatedItemCard = ({ pID }) => {
     request
       .getStyles(pID)
       .then((data) => {
+        // console.log('styles data:', data.data);
         setPhotoData(data.data.results[0].photos[0].url);
         if (data.data.results[0].sale_price) {
           setPrice(data.data.results[0].sale_price);
@@ -30,6 +33,7 @@ const RelatedItemCard = ({ pID }) => {
     request
       .getProductInfo(pID)
       .then((data) => {
+        // console.log('prod info:', data.data);
         setProductData(data.data);
       })
       .catch((err) => {
@@ -45,30 +49,54 @@ const RelatedItemCard = ({ pID }) => {
       .catch((err) => {
         console.log("This is a RelatedItemCard error:", err);
       });
+    request
+      .getProductInfo(ogID)
+      .then((data) => {
+        // console.log('prod info:', data.data);
+        setOVItem(data.data);
+      })
+      .catch((err) => {
+        console.log("This is a RelatedItemCard error:", err);
+      });
+    request
+      .getStyles(ogID)
+      .then((data) => {
+        // console.log('styles data:', data.data);
+        if (data.data.results[0].sale_price) {
+          setOVItemPrice(data.data.results[0].sale_price);
+        } else {
+          setOVItemPrice(data.data.results[0].original_price);
+        }
+      })
+      .catch((err) => {
+        console.log("This is a RelatedItemCard error:", err);
+      });
   }, []);
 
   // need little star button -- Might have to grab from font awesome or something similar -- done
   //on click, the star button opens a pop-up window that shows a comparison between the item on the current overview and the item on the related list.
 
-  const handlePopToggle = (event) => {
+  const handlePopToggle = (e) => {
     //do stuff. make popup window.
-    event.preventDefault();
+    e.preventDefault();
     setPop(true);
   }
 
+  console.log('pop:', pop);
+
   return (
     <div>
-      <aside>
-        <img src={photoData} height="175" />
+      <aside className="RC_card">
+        <img className="RC_card_photo" src={photoData} height="225" />
         <i className="fa-solid fa-star" onClick={handlePopToggle}></i>
-        <h6>{productData.category}</h6>
-        <h5>{productData.name}</h5>
+        <h6 className="RC_product_category">{productData.category}</h6>
+        <h5 className="RC_product_name">{productData.name}</h5>
         <p>
-          <small>{price}</small>
+          <small className="RC_product_price">{price}</small>
         </p>
         <Stars rating={starRating} />
       </aside>
-      {pop ? <ComparisonModal toggle={handlePopToggle} overviewItem={pID} relatedItem={productData} /> : null}
+      {pop ? <ComparisonModal toggle={setPop} overviewItem={ovItem} relatedItem={productData} relatedPrice={price} ovPrice={ovItemPrice}/> : null}
     </div>
 
   );
