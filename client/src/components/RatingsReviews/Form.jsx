6b8e8b29b42ccd169c1402677be248b3b50165ee
Thach Do <thachdo90@gmail.com>
2022-08-29 17:10:38 -0700

    Refactored logic for generating star rating
    Instead of writing each star directly, we now generate those 5 star options by mapping an array [poor, fair, average, good, great] onto an array of stars that have the same functionality as before

--- client/src/components/RatingsReviews/Form.jsx
+++ client/src/components/RatingsReviews/Form.jsx
@@ -17,18 +17,19 @@ const Form = ({ productName }) => {
     <form>
       <h3>Write your review</h3>
       <p>About the {productName}</p>
+      {/* there's a lot of redundant logic here, this could be refactored later, maybe generate it with a for loop */}
       <div id="RR-overall-rating">
         <p>Overall Rating:</p><span aria-label="required">* </span><br/>
-        <input type="radio" id="1" name="rating" value="1" required onClick={changeRating} />
-        <label for="1"><img src={rating >=1 ? fullStar : noStar} onMouseOver={() => tempRating(1)}/></label>
-        <input type="radio" id="2" name="rating" value="2" onClick={changeRating} />
-        <label for="2"><img src={rating >=2 ? fullStar : noStar} onMouseOver={() => tempRating(2)}/></label>
-        <input type="radio" id="3" name="rating" value="3" onClick={changeRating} />
-        <label for="3"><img src={rating >=3 ? fullStar : noStar} onMouseOver={() => tempRating(3)}/></label>
-        <input type="radio" id="4" name="rating" value="4" onClick={changeRating} />
-        <label for="4"><img src={rating >=4 ? fullStar : noStar} onMouseOver={() => tempRating(4)} /></label>
-        <input type="radio" id="5" name="rating" value="5" onClick={changeRating} />
-        <label for="5"><img src={rating >=5 ? fullStar : noStar} onMouseOver={() => tempRating(5)}/></label>
+        {[null, 'Poor', 'Fair', 'Average', 'Good', 'Great'].map((element, index) => {
+          if (element) {
+            return(
+              <div>
+                <input type="radio" id={index} name="rating" value={index} required onClick={changeRating} />
+                <label for={index}><img src={rating >= index ? fullStar : noStar} onMouseOver={() => tempRating(index)}/></label><span>{element}</span>
+              </div>
+            )
+          }
+        })}
       </div>
       <div id="RR-form-recommend">Do you recommend this product?<span aria-label="required">* </span><br/>
         <input type="radio" id="yes" name="recommend" value="yes" checked required />
