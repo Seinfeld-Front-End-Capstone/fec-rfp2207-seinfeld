b5b10a4b3c3fca4acd7d9b52191c3b0861e274a4
Thach Do <thachdo90@gmail.com>
2022-09-01 10:44:57 -0700

    add review form pops up and blocks user from clicking on anythign else on the page

--- client/src/components/RatingsReviews/Form/Form.jsx
+++ client/src/components/RatingsReviews/Form/Form.jsx
@@ -84,50 +84,53 @@ const Form = ({ productName, productId }) => {
   }
 
   return (
-    <form id="RR-form">
-      <h3>Write your review</h3>
-      <p>About the {productName}</p>
+    <div>
+      <div id="RR-form-bg"></div>
+      <form id="RR-form">
+        <h3>Write your review</h3>
+        <p>About the {productName}</p>
 
-      <div id="RR-overall-rating">
-        <p>Overall Rating: {requiredTag}</p>
-        <div className="RR-rate-star">
-          {starRatings}{rated && <span>{ratings[rating]}</span>}
+        <div id="RR-overall-rating">
+          <p>Overall Rating: {requiredTag}</p>
+          <div className="RR-rate-star">
+            {starRatings}{rated && <span>{ratings[rating]}</span>}
+          </div>
         </div>
-      </div>
 
-      <div id="RR-form-recommend">Do you recommend this product? {requiredTag}<br/>
-        <input type="radio" id="yes" name="recommend" value="yes" checked required />
-        <label for="yes">yes</label>
-        <input type="radio" id="no" name="recommend" value="no" />
-        <label for="no">no</label>
-      </div>
+        <div id="RR-form-recommend">Do you recommend this product? {requiredTag}<br/>
+          <input type="radio" id="yes" name="recommend" value="yes" checked required />
+          <label for="yes">yes</label>
+          <input type="radio" id="no" name="recommend" value="no" />
+          <label for="no">no</label>
+        </div>
 
-      {chars ? <ProdChars chars={chars}/> : null}
-      {/* {chars && <ProdChars chars={chars}/>} */}
+        {chars ? <ProdChars chars={chars}/> : null}
+        {/* {chars && <ProdChars chars={chars}/>} */}
 
-      <p>Summary:</p>
-      <input id="RR-summary" placeholder="Example: Best purchase ever!" maxLength="60" size="50" name="summary" ></input><br/>
+        <p>Summary:</p>
+        <input id="RR-summary" placeholder="Example: Best purchase ever!" maxLength="60" size="50" name="summary" ></input><br/>
 
-      <p>Review:</p>
-      <textarea id="RR-body" placeholder="Why did you like the product or not?" minLength="50" maxLength="1000" name="body" onChange={countChar}
-      rows="10" cols="44" required></textarea><br/>
-      <p>{bodyCharCount < 50 ? `Minimum required characters left: ${50 - bodyCharCount}` : 'Minimum reached'}</p>
+        <p>Review:</p>
+        <textarea id="RR-body" placeholder="Why did you like the product or not?" minLength="50" maxLength="1000" name="body" onChange={countChar}
+        rows="10" cols="44" required></textarea><br/>
+        <p>{bodyCharCount < 50 ? `Minimum required characters left: ${50 - bodyCharCount}` : 'Minimum reached'}</p>
 
-      <label for="upload-photos">Upload photos: </label>
-      {photos.length < 5 && <input type="file" id="upload-photos" name="photos" accept="image/*" onChange={handleImageUpload}/>}
-      <br/>
-      {photos.map(photo => <UploadedPhotos photo={photo}/>)}
+        <label for="upload-photos">Upload photos: </label>
+        {photos.length < 5 && <input type="file" id="upload-photos" name="photos" accept="image/*" onChange={handleImageUpload}/>}
+        <br/>
+        {photos.map(photo => <UploadedPhotos photo={photo}/>)}
 
-      <p>What is your nickname:  {requiredTag}</p>
-      <input id="RR-nickname" placeholder="Example: jackson11!" maxLength="60" name="name" required />
-      <p>For privacy reasons, do not use your full name or email address</p>
+        <p>What is your nickname:  {requiredTag}</p>
+        <input id="RR-nickname" placeholder="Example: jackson11!" maxLength="60" name="name" required />
+        <p>For privacy reasons, do not use your full name or email address</p>
 
-      <p>Your Email: {requiredTag}</p>
-      <span><input placeholder="Example: jackson11@email.com" maxLength="60" name="email"/></span>
-      <p>For authentication reasons, you will not be emailed</p>
+        <p>Your Email: {requiredTag}</p>
+        <span><input placeholder="Example: jackson11@email.com" maxLength="60" name="email"/></span>
+        <p>For authentication reasons, you will not be emailed</p>
 
-      <input type="button" value="submit" onClick={handleSubmit}/>
-    </form>
+        <input type="button" value="submit" onClick={handleSubmit}/>
+      </form>
+    </div>
   )
 }
 
