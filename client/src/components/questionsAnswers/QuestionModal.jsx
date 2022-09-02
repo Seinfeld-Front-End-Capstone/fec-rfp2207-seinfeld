68a5d0a3358c81b7c50ee4585deee48b6aee3472
Daniel Tan <dantan510@gmail.com>
2022-09-01 19:29:36 -0700

    added function to post question, unfinished

--- client/src/components/questionsAnswers/QuestionModal.jsx
+++ client/src/components/questionsAnswers/QuestionModal.jsx
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import please from '../.././request.js';
 
-const QuestionModal = ({modal, onClose}) => {
+const QuestionModal = ({modal, onClose, productId, setInitial, setQuestion}) => {
+  const [userQuestion, setUserQuestion] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const asterisk = <span id='qa-asterisk'>*</span>
-  if (!modal) return null
+
+  // send post request to add to api?
+  // send get request to grab new list of questions
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let splitEmail = email.split('@')
+    if (splitEmail.length !== 2 && !splitEmail[1].includes('.')) {
+      alert('warning message')
+      return;
+    }
+
+    please.addQuestion({body: userQuestion, name: name, email: email, product_id: productId})
+    .then(() => {
+      return please.getQuestions(productId)
+    })
+    .then((data) => {
+      console.log('data please: ', data);
+      // sort data first
+      // set initial and question states to new get request
+    })
+  }
+
+  if (modal)
   return (
     <div className='qa-question-modal'>
       <p onClick={onClose} className='close-button'>X</p>
         <h3>Ask Your Question!</h3>
         <h5>About the [product name] Name</h5>
 
-        <div>
-          <p>Your Question{asterisk}</p>
-          <input type='text' maxLength='1000'></input>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <div>Your Question{asterisk}</div>
+            <input type='text' maxLength='1000' onChange={(e) => setUserQuestion(e.target.value)} required></input>
+          </div>
 
-        <div>
-          <p>What is your nickname{asterisk}</p>
-          <input type='text' placeholder='Example: jackson11!' maxLength='60'></input>
-          <div>For privacy reasons, do not use your full name or email address</div>
-        </div>
+          <div>
+            <div>What is your nickname{asterisk}</div>
+            <input type='text' placeholder='Example: jackson11!' maxLength='60' onChange={(e) => setName(e.target.value)} required></input>
+            <div>For privacy reasons, do not use your full name or email address</div>
+          </div>
 
-        <div>
-          <p>Your email{asterisk}</p>
-          <input type='text' placeholder='Why did you like the product or not?'></input>
-          <div>For authentication reasons, you will not be emailed</div>
-        </div>
+          <div>
+            <div>Your email{asterisk}</div>
+            <input type='text' placeholder='Why did you like the product or not?' onChange={(e) => setEmail(e.target.value)} required></input>
+            <div>For authentication reasons, you will not be emailed</div>
+          </div>
 
-        <button>Submit Question</button>
+          <button>Submit Question</button>
+        </form>
+
     </div>
   )
 }
