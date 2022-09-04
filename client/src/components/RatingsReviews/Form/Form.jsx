29a50ca017b94ed0de78b3030acb8c730fbbb140
Thach Do <thachdo90@gmail.com>
2022-09-04 11:04:01 -0700

    finished styling form

--- client/src/components/RatingsReviews/Form/Form.jsx
+++ client/src/components/RatingsReviews/Form/Form.jsx
@@ -16,8 +16,10 @@ const Form = ({ productName, productId, toggleForm, refreshReviews }) => {
   const [photos, setPhotos] = useState([]);
   const [chars, setChars] = useState([]);
   const [myWidget, setMyWidget] = useState(null);
+  //could refactor to store as one state?
   const [cancelBtnActive, setCancelBtnActive] = useState(false);
   const [submitBtnActive, setSubmitBtnActive] = useState(false);
+  const [uploadBtnActive, setUploadBtnActive] = useState(false);
 
   useEffect(() => {
     please.getReviewMeta(productId)
@@ -93,14 +95,14 @@ const Form = ({ productName, productId, toggleForm, refreshReviews }) => {
   }
 
   const ratings = [null, 'Poor', 'Fair', 'Average', 'Good', 'Great'];
+
   const starRatings = ratings.map((element, index) => {
     if (element) {
       return(
-        <React.Fragment key={index}>
+        <span key={index} className="RR-rate-star">
           <input type="radio" id={index} name="rating" value={index} required onClick={changeRating} />
           <label htmlFor={index} className="Stars" onMouseEnter={() => tempRating(index)} onMouseLeave={() => tempRating(0)} style={{'--percent': rating >= index ? '100%' : '0%'}}>★</label>
-
-        </React.Fragment>
+        </span>
       )
     }
   })
@@ -122,10 +124,9 @@ const Form = ({ productName, productId, toggleForm, refreshReviews }) => {
         </div>
         <form>
           <div id="RR-overall-rating" className="RR-form-fields">
-            <span>Overall Rating: {requiredTag}</span>
-            <div className="RR-rate-star">
-              {starRatings}{rated && <span>{ratings[rating]}</span>}
-            </div>
+            <div>Overall Rating: {requiredTag}</div>
+            <div>{starRatings}</div>
+            {rated && <div>{ratings[rating]}</div>}
           </div>
 
           <div id="RR-form-recommend" className="RR-form-fields">Do you recommend this product? {requiredTag}<br/>
@@ -143,16 +144,14 @@ const Form = ({ productName, productId, toggleForm, refreshReviews }) => {
           </div>
 
           <div className="RR-form-fields">
-            <div>Review:</div>
+            <div>Review: {requiredTag}</div>
             <textarea id="RR-body" placeholder="Why did you like the product or not?" minLength="50" maxLength="1000" name="body" onChange={countChar}
             rows="10" cols="44" required></textarea><br/>
             <div>{bodyCharCount < 50 ? `Minimum required characters left: ${50 - bodyCharCount}` : 'Minimum reached'}</div>
           </div>
 
           <div className="RR-form-fields">
-            {photos.length <5 && <button onClick={() => {
-              myWidget.open();
-            }}>Upload photos</button>}
+            {photos.length <5 && <button onClick={() =>myWidget.open()} onMouseEnter={() => setUploadBtnActive(true)} onMouseLeave={() => setUploadBtnActive(false)} className={uploadBtnActive ? 'active' : ''}>upload photos</button>}
             <div id="RR-photo-uploads">
               {photos.map(photo => <img src={photo.thumbnail} alt="Your uploaded photo of the product"/>)}
             </div>
@@ -161,13 +160,13 @@ const Form = ({ productName, productId, toggleForm, refreshReviews }) => {
           <div className="RR-form-fields">
             <div>What is your nickname:  {requiredTag}</div>
             <input id="RR-nickname" placeholder="Example: jackson11!" maxLength="60" name="name" required />
-            <div>For privacy reasons, do not use your full name or email address</div>
+            <div className="RR-disclaimer">For privacy reasons, do not use your full name or email address</div>
           </div>
 
           <div className="RR-form-fields">
             <div>Your Email: {requiredTag}</div>
             <span><input placeholder="Example: jackson11@email.com" maxLength="60" name="email"/></span>
-            <div>For authentication reasons, you will not be emailed</div>
+            <div className="RR-disclaimer">For authentication reasons, you will not be emailed</div>
           </div>
         </form>
       </div>
