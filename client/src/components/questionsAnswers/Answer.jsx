6315ff3a077fc69728e8058e339b20fd37b77fe7
Daniel Tan <dantan510@gmail.com>
2022-09-05 12:53:33 -0700

    added catch errors, answer list displays seller at the top, use moment to format date

--- client/src/components/questionsAnswers/Answer.jsx
+++ client/src/components/questionsAnswers/Answer.jsx
@@ -1,37 +1,29 @@
 import React, {useState} from 'react';
 import please from '../.././request.js';
+import moment from 'moment';
+import widget from '../helpers/widget.js';
 
 const Answer = ({answer}) => {
   // console.log('answers props : ', answer)
   const [answerCount, setAnswerCount] = useState(answer.helpfulness)
-  const [isReported, setReported] = useState('Report')
+  const [isReported, setReported] = useState('Report');
+  const [photos, setPhotos] = useState('');
 
-  // let q = props.q;
-  // let keys = Object.keys(q.answers)[0];
-  // let answer = q.answers[keys];
-  // if (!answer) {
-  //   return <div>No more answers!</div>
-  // }
-  let date = answer.date.slice(0, 10);
+  const formattedDate = moment(answer.date).format('ll');
 
-  // console.log('answer : ', answer);
   const handleCount = () => {
     if (answer.helpfulness === answerCount) {
       please.markAnswerAsHelpful(answer.answer_id)
-      .then(() => {
-        // console.log('success')
-        setAnswerCount((count) => count + 1)
-      })
+      .then(() => setAnswerCount((count) => count + 1))
+      .catch((err) => console.log(err))
     }
   }
 
   const handleClick = () => {
     if (isReported === 'Report') {
       please.reportAnswer(answer.answer_id)
-      .then(() => {
-        // console.log('success')
-        setReported('Reported')
-      })
+      .then(() => setReported('Reported'))
+      .catch((err) => console.log(err))
     }
   }
 
@@ -40,7 +32,7 @@ const Answer = ({answer}) => {
       <span id='qa-A'>A:</span> {answer.body}
       <div className='qa-answer-body'>
         <div id='qa-answer-name'>
-          by {answer['answerer_name']}, {date}
+          by {answer['answerer_name']}, {formattedDate}
           <span id='qa-answer-helpful'>
           <span className='qa-line'>|</span> Helpful?
           <span onClick={handleCount}>  Yes ({answerCount})</span>
