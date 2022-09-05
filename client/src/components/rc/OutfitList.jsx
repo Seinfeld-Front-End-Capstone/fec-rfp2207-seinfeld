3de51c14e0b66f1eb884fff6f7d13bfd55127c0a
Lynn <griff.lynn8@gmail.com>
2022-09-05 14:13:03 -0700

    added previous button functionality for Outfit list and fixed bug that would shift the first item off the list when next button was clicked, even if there wasn't a next item to view on the list.

--- client/src/components/rc/OutfitList.jsx
+++ client/src/components/rc/OutfitList.jsx
@@ -24,23 +24,23 @@ const OutfitList = ({ id }) => {
     setCardCount(prevCount => prevCount + 1);
   }
 
-  //when a card is added to the outfit list, also add it to display. after 3 cards are added, don't add any more to display unless > button is clicked. Then, push the next card in line to the display. if < is clicked, unshift the card that comes before whatever the card at [0] is. That way, only 3 cards will be shown at once. The add card should stay in place at all times. // if card exists within the outfit array AND the display array, display it.
-
-  console.log('display:', outfitDisplay);
   const handleNextClick = (e) => {
-    // there should only ever be 3 outfit cards displayed at a time.
     e.preventDefault();
-    //find the value of the last element in the display array,
-      //check to find the same in the outfit array,
-      var next = outfitDisplay[2] + 1;
-      console.log('next:', next);
-      // pushing the next element of outfit array onto the end of display array
+    var next = outfitDisplay[2] + 1;
+    if (outfit.indexOf(next) !== -1) {
       setOutfitDisplay(outfitDisplay.filter((item) => item !== outfitDisplay[0]));
       setOutfitDisplay(prevDisplay => [...prevDisplay, next]);
-      //shift the first element in display out of the array while
-      console.log('display:', outfitDisplay);
+    }
   }
 
+  const handlePrevClick = (e) => {
+    e.preventDefault();
+    var prev = outfitDisplay[0] - 1;
+    if (outfit.indexOf(prev) !== -1) {
+      setOutfitDisplay(outfitDisplay.filter((item) => item !== outfitDisplay[2]));
+      setOutfitDisplay(prevDisplay => [prev, ...prevDisplay]);
+    }
+  }
 
 
   return (
@@ -48,7 +48,7 @@ const OutfitList = ({ id }) => {
       <h5>Your Outfit</h5>
       <button
       className="RC_prev"
-      onClick={null}>
+      onClick={handlePrevClick}>
         {"<"}
       </button>
       <ul className="RC_outfit_list">
