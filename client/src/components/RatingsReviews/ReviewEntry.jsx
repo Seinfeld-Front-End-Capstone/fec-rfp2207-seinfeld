8fc498a925426d2e7431f477d0fb19c1646ad4b7
Thach Do <thachdo90@gmail.com>
2022-09-06 16:24:06 -0700

    users can not vote yes on helpfullness of a review

--- client/src/components/RatingsReviews/ReviewEntry.jsx
+++ client/src/components/RatingsReviews/ReviewEntry.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stars from '../helpers/Stars.jsx';
 import moment from 'moment';
 import Photos from './Photos.jsx';
 import { FaCheckCircle } from 'react-icons/fa';
+import please from '../../request.js';
 
 const ReviewEntry = ({ review, recommended }) => {
   let { review_id, body, date, helpfulness, photos, rating, recommend, response, reviewer_name, summary } = review;
 
-const formattedDate = moment(date).format('ll');
+  const [voted, setVoted] = useState(false);
+
+  const formattedDate = moment(date).format('ll');
+
+  const voteYes = (e) => {
+    please.markReviewAsHelpful(review_id)
+    .then(() => {
+      setVoted(true);
+      e.target.classList.add('bold');
+    })
+    .catch(err => console.log(err));
+  }
 
   return (
     <div className="RR-review-ctn">
@@ -22,7 +34,10 @@ const formattedDate = moment(date).format('ll');
       <p>{body}</p>
       {recommend ? <p><FaCheckCircle /> I recommend this product</p> : null}
       {response ? <p className="RR-seller-response">Response from seller: {response}</p> : null}
-      <p>Helpful? <span>Yes</span> ({helpfulness})</p>
+      <p>Helpful? <span
+      className='underline'
+      onClick={voted ? null : voteYes}
+      >Yes ({helpfulness})</span></p>
       <div id="RR-photos-ctn">
         {photos && photos.map(photo => <Photos key={photo.id} photo={photo}/>)}
       </div>
