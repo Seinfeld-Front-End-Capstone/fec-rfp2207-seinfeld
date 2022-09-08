ac6dd7a2db13cfa52e6301110c94e3cc2c8256e3
Thach Do <thachdo90@gmail.com>
2022-09-07 20:14:37 -0700

    installed msw to mock server

--- client/src/components/__tests__/RatingsReviews.test.js
+++ client/src/components/__tests__/RatingsReviews.test.js
@@ -1,14 +1,27 @@
+import React from 'react';
+import { setupWorker, rest } from 'msw';
+import { setupServer } from 'msw/node';
 import {render, fireEvent, screen} from '@testing-library/react'
 import RatingsReviews from '../RatingsReviews/RatingsReviews.jsx';
 
-
 describe('Draft test', () => {
-  it('tests something', () => {
-    render(<RatingsReviews productId={'65631'}/>);
-
-    const headingText = screen.getAllByText(/review/i)
-    expect(true).toBe(true);
+  it('should always have an "add a review" button', () => {
+    render(<RatingsReviews productId={65631}/>);
+    const headingText = screen.getAllByText(/Add a review/i);
     expect(headingText).toBeTruthy();
+  })
+
+  it('should prompt users to add a review when there are none', () => {
+    render(<RatingsReviews productId={0}/>)
+    expect(screen.getAllByText(/Add a review/i)).toBeTruthy;
+    expect(screen.getByText(/add a review/i)).toBeTruthy;
+    expect(screen.getAllByText("Be the first to review this product!").toBeTruthy);
+    let button = screen.getByRole('button');
+    expect(button).toBeTruthy;
+  })
+
+  it('should open up a form', () => {
+    render(<RatingsReviews productId={65631}/>);
   })
 })
 
